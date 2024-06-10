5e77bfea21510a529d10b80ab8fdb7cff5d4c0bd
JHeok <skdhd999@gmail.com>
2024-06-10 11:11:29 +0900

    delete author

--- process_delete_author.php
+++ process_delete_author.php
@@ -6,6 +6,9 @@ $filtered = array(
     'id' => mysqli_real_escape_string($conn,$_POST['id']),
 );
 
+$sql = "delete from topic where author_id = {$filtered['id']}";
+mysqli_query($conn, $sql);
+
 $sql = "delete from author where id = {$filtered['id']}";
 $result = mysqli_query($conn, $sql);
 
